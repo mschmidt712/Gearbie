176600eee436ac890e4405e4d6f293bd22ac33dd
Marie Schmidt <marie.schmidt.712@gmail.com>
2016-09-21 17:11:40 -0600

    KM-472-Explore-Page: Build explore page and added responsive design.

---
+++ src/components/shared/FooterComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () =>
+  (<div className="page-footer">
+    <div className="arrow-link">
+      <i className="arrow-icon material-icons orange md">ic_arrow_downward</i>
+    </div>
+    <h5 className="footer-text"> Explore. Contribute. Code</h5>
+  </div>);
+
+export default Footer;
