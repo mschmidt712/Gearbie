fd98d4d98f9cb0074941bba53b40a8d7c44a7cd2
Alex Uribe <auribe@kenzan.com>
2016-09-22 16:11:00 -0600

    Header responsiveness and logos

--- src/components/shared/HeaderComponent.js
+++ src/components/shared/HeaderComponent.js
@@ -5,10 +5,28 @@ const Header = () =>
   (<div>
     <div className="navbar">
       <ul>
-        <li><a href="a">Explore</a></li>
-        <li><a href="a">About</a></li>
-        <li><a href="a">Blog</a></li>
-        <li><a href="a">Connect</a></li>
+        <div className="navbar-links menu-items">
+          <img src="./src/assets/Kenzan_Mark_PMS877.svg" alt="Kenzan Logo" />
+          <li><a href="a">Explore</a></li>
+          <li><a href="a">About</a></li>
+          <li><a href="a">Blog</a></li>
+          <li><a href="a">Connect</a></li>
+        </div>
+        <div className="navbar-social-media">
+          <img src="./src/assets/Twitter.png" alt="linked-in" />
+          <img src="./src/assets/Linkedin.png" alt="linked-in" />
+          <img src="./src/assets/Github.png" alt="linked-in" />
+          <img src="./src/assets/Facebook.png" alt="linked-in" />
+        </div>
+        <div className="navbar-links nav-menu">
+          <li className="mobile-nav"><a>Menu</a></li>
+          <div className="dropdown-content">
+            <li><a href="a">Explore</a></li>
+            <li><a href="a">About</a></li>
+            <li><a href="a">Blog</a></li>
+            <li><a href="a">Connect</a></li>
+          </div>
+        </div>
       </ul>
     </div>
   </div>
