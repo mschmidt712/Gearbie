b68183571eb2cf43bb7deeefd81f34eca84b3212
Marie Schmidt <marie.schmidt.712@gmail.com>
2016-10-11 11:02:17 -0600

    KM-472-Responsive-Nav-Bar

--- src/components/shared/HeaderComponent.js
+++ src/components/shared/HeaderComponent.js
@@ -1,36 +1,143 @@
 import React, { PropTypes } from 'react';
 import { Link } from 'react-router';
+import FontAwesome from 'react-fontawesome';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+import HeaderMenuDropdown from './HeaderMenuDropdown';
+import SocialMenuDropdown from './SocialMenuDropdown';
 
 /**
  * The App Header including navigation and social media links.
  */
-const Header = ({ clickEvent }) =>
-(<div className="navbar">
-  <ul>
-    <div className="navbar-links menu-items">
-      <Link to="/" onClick={clickEvent}>
-        <img src="/assets/Kenzan_Mark_PMS877.svg" alt="Kenzan Logo" />
-      </Link>
-      <li><Link to="/open-source" activeClassName="active" onClick={clickEvent}>Explore</Link></li>
-      <li><Link to="/kenzan" activeClassName="active" onClick={clickEvent}>About</Link></li>
-      <li><Link to="/blog" activeClassName="active" onClick={clickEvent}>Blog</Link></li>
-      <li><Link to="/connect" activeClassName="active" onClick={clickEvent}>Connect</Link></li>
-    </div>
-    <div className="navbar-social-media">
-      <img src="/assets/Twitter.png" alt="linked-in" />
-      <img src="/assets/Linkedin.png" alt="linked-in" />
-      <img src="/assets/Github.png" alt="linked-in" />
-      <img src="/assets/Facebook.png" alt="linked-in" />
-    </div>
-  </ul>
-</div>
-);
+class HeaderComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showMenu: false,
+      showSocial: false,
+    };
+    this.showMenu = this.showMenu.bind(this);
+    this.showSocial = this.showSocial.bind(this);
+    this.clearBoth = this.clearBoth.bind(this);
+    this.render = this.render.bind(this);
+  }
 
-Header.propTypes = {
+  showMenu() {
+    this.setState({
+      showMenu: !this.state.showMenu,
+    });
+    this.render();
+  }
+
+  showSocial() {
+    this.setState({
+      showSocial: !this.state.showSocial,
+    });
+    this.render();
+  }
+
+  clearBoth() {
+    this.setState({
+      showSocial: false,
+      showMenu: false,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="navbar">
+          <ul>
+            <div className="navbar-links menu-items full-nav">
+              <Link to="/" onClick={this.props.clickEvent}>
+                <img className="kenzan-logo" src="/assets/KenzanLogo1.svg" alt="Kenzan Logo" />
+              </Link>
+              <li>
+                <Link
+                  to="/open-source"
+                  activeClassName="active-nav"
+                  onClick={this.props.clickEvent}
+                >
+                  Explore
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/kenzan"
+                  activeClassName="active-nav"
+                  onClick={this.props.clickEvent}
+                >
+                  About
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/blog"
+                  activeClassName="active-nav"
+                  onClick={this.props.clickEvent}
+                >
+                  Blog
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/connect"
+                  activeClassName="active-nav"
+                  onClick={this.props.clickEvent}
+                >
+                  Connect
+                </Link>
+              </li>
+            </div>
+            <div className="navbar-social-media">
+              <a href="http://www.twitter.com">
+                <img src="/assets/Twitter.png" alt="Twitter" />
+              </a>
+              <a href="http://www.linkedin.com">
+                <img src="/assets/Linkedin.png" alt="Linkedin" />
+              </a>
+              <a href="http://www.github.com">
+                <img src="/assets/Github.png" alt="Github" />
+              </a>
+              <a href="www.facebook.com">
+                <img src="/assets/Facebook.png" alt="Facebook" />
+              </a>
+            </div>
+            <div className="mobile-nav-container">
+              <button className="nav-menu mobile-nav no-button" onClick={this.showMenu}>
+                Menu
+              </button>
+              <FontAwesome
+                name="share-alt"
+                className="nav-menu mobile-nav mobile-social-media no-button"
+                onClick={this.showSocial}
+              />
+            </div>
+          </ul>
+        </div>
+        <ReactCSSTransitionGroup
+          transitionName="mobile-menu-animation"
+          transitionEnterTimeout={200}
+          transitionLeaveTimeout={200}
+        >
+          {this.state.showMenu && <HeaderMenuDropdown clearBoth={this.clearBoth} />}
+        </ReactCSSTransitionGroup>
+        <ReactCSSTransitionGroup
+          transitionName="mobile-social-animation"
+          transitionEnterTimeout={200}
+          transitionLeaveTimeout={200}
+        >
+          {this.state.showSocial && <SocialMenuDropdown clearBoth={this.clearBoth} />}
+        </ReactCSSTransitionGroup>
+      </div>
+    );
+  }
+}
+
+HeaderComponent.propTypes = {
     /**
    * A functions that sends click events to the app component for conditional animations.
    */
   clickEvent: PropTypes.func.isRequired,
 };
 
-export default Header;
+export default HeaderComponent;
