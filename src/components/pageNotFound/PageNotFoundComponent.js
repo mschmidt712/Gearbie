af78e1a5b02d80779c6407d108f5c2fc6ad8e565
Marie Schmidt <marie.schmidt.712@gmail.com>
2016-10-20 13:42:25 -0600

    KM-Default-Route: added default route for page not found.

---
+++ src/components/pageNotFound/PageNotFoundComponent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function PageNotFound(props) {
+  console.log(props);
+  return (
+    <div className="page-container">
+      <h1> Page Not Found </h1>
+      <h5> Please check the URL and try again </h5>
+    </div>
+  );
+}
+
+export default PageNotFound;
